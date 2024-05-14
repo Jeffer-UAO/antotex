76ed8c16c9ee6427a996c993788231df40cfea06
JefferDevop <somadesarrollo@gmail.com>
2024-05-14 16:52:39 -0500

    My first commit

--- src/hooks/useCategory.js
+++ src/hooks/useCategory.js
@@ -1,16 +1,22 @@
 import { useState } from "react";
 
-import { getCategoriesApi } from "../api/category";
+import { Categories } from "../api/category";
+
+
+
 
 export function useCategory() {
+
+  const categoryCtrl = new Categories()
   const [loadingCategory, setLoadingCategory] = useState(true);
   const [error, setError] = useState(null);
   const [categories, setCategories] = useState(null);
 
   const getCategories = async () => {
+
     try {
       setLoadingCategory(true);
-      const response = await getCategoriesApi();
+      const response = await categoryCtrl.getAll();
       setLoadingCategory(false);
       setCategories(response);
     } catch (error) {

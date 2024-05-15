097241d3fb0e21f9ada292dd1b60d7093a7ac7eb
JefferDevop <somadesarrollo@gmail.com>
2024-05-15 02:44:03 -0500

    trhir commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import NextNprogress from "nextjs-progressbar";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../scss/global.scss";
 import { CartProvider } from "@/contexts";
@@ -78,13 +78,7 @@ export default function App(props) {
 
   return (
     <>
-      <NextNprogress
-        color="#FF0"
-        startPosition={0.3}
-        stopDelayMs={200}
-        height={10}
-      />
-
+ 
       <CartProvider>
         <Component {...pageProps} />
         <ToastContainer
